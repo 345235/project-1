5a112f98b45cc1e5927543d7ebd09cbd93f02bf9
Paul Haider <paul@kangaroos.at>
2026-05-01 15:01:33 +0200

    lol

---
+++ kalender.js
@@ -0,0 +1,33 @@
+const monthYearElement = document.getElementById ('monthYear')
+const datesElement = document.getElementById ('dates')
+const prevBtn = document.getElementById ('prevBtn')
+const nextBtn = document.getElementById ('nextBtn')
+
+let currentDate = new Date ();
+
+const updateCalendar = () => {
+  const currentYear = currentDate.getFullYear ();
+    const currentMonth = currentDate.getMonth ();
+      const firstDay = new Date(currentYear, currentMonth,0);
+       const lastDay = new Date (currentYear, currentMonth+1, 0);
+        const totalDays  = lastDay.getDate ();
+         const firstDayIndex = firstDay.getDay();
+           const lastDayIndex = lastDay.getDay();
+
+
+  const monthYearString = currentDate.toLocaleString('defult',{month: 'long', year: 'numeric'});
+   monthYearElement.textContent = monthYearString;
+   let datesHTML = '';
+     
+
+   for  (let i = firstDayIndex; i > 0; i--) {
+    const prevDate = new Date (currentYear,currentMonth, 0
+   - i + 1 );
+   datesHTML += `<div class="date inactive">${prevDate.getDate()} </div>`;
+
+   }
+ 
+
+
+
+}
